b3c5452dc8479c5d8b473eb4b8bb4abfe5fce970
iambast <ambastaryan18@gmail.com>
2025-07-23 10:40:22 +0530

    Update Login.jsx

--- frontend/vite-project/src/pages/Login.jsx
+++ frontend/vite-project/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/admin/login', {
+      const res = await axios.post('https://disaster-management-backend-5sdj.onrender.com/api/admin/login', {
         username,
         password,
       });
